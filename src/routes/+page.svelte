cc1344b30e86e6fde616243cb8b421da17746da7
陈刑 <dev.chenxing@gmail.com>
2025-11-14 15:14:21 +0800

    refactor(page): update main layout with custom styles and content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
-<main class="mx-auto prose prose-invert">
-	<h1>Welcome to SvelteKit</h1>
-	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-	</p>
+<main
+	class="relative grid min-h-[90svh] items-center justify-items-start px-6 sm:px-10 md:min-h-screen md:px-16 lg:px-24 xl:px-32"
+>
+	<div>
+		<h1 class="text-[clamp(3.5rem,10vw,16rem)] leading-[1.1] text-white">
+			<span class="block">祝吴双</span>
+			<span class="block">生日快乐</span>
+		</h1>
+		<p class="mt-8">
+			<a
+				href="https://github.com/chenxing-dev"
+				class="text-white/40 underline underline-offset-4 transition-colors hover:text-white/60"
+			>
+				@陈刑
+			</a>
+		</p>
+	</div>
 </main>
