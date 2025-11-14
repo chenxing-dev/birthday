97f1eac6c935dc659eac17e5a1abd0b2c0e8883e
陈刑 <dev.chenxing@gmail.com>
2025-11-14 15:29:58 +0800

    feat: add Noto Sans SC font and update styles in app.css and +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	class="relative grid min-h-[90svh] items-center justify-items-start px-6 sm:px-10 md:min-h-screen md:px-16 lg:px-24 xl:px-32"
 >
 	<div>
-		<h1 class="text-[clamp(3.5rem,10vw,16rem)] leading-[1.1] text-white">
+		<h1 class="font-sans text-[clamp(3.5rem,10vw,16rem)] leading-[1] text-white">
 			<span class="block">祝吴双</span>
 			<span class="block">生日快乐</span>
 		</h1>
