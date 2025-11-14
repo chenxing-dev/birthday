50e548653d519c6bc0ab480ecceb50d0576b83b0
陈刑 <dev.chenxing@gmail.com>
2025-11-14 17:49:58 +0800

    fix(page): update styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
-<div class="prose max-w-none prose-invert">
+<div class="prose max-w-none prose prose-zinc prose-invert">
 	<main
 		class="relative grid min-h-[90svh] items-end justify-items-start px-6 sm:px-10 md:min-h-screen md:items-center md:px-16 md:pb-0 lg:px-24 xl:px-32"
 	>
-		<div class="not-prose">
-			<h1 class="font-sans text-[clamp(5.5rem,18vw,16rem)] leading-[1.05] font-semibold text-white">
+		<div>
+			<h1 class="text-[clamp(5.5rem,18vw,16rem)] leading-[1.05] font-semibold">
 				<span class="block">祝</span>
 				<span class="block">吴双</span>
 				<span class="block">生日快乐</span>
 			</h1>
 		</div>
 	</main>
-	<footer class="px-6 pb-6 text-xs text-white/40 sm:px-10 md:px-16 lg:px-24 xl:px-32">
-		<a href="https://github.com/chenxing-dev" class="transition-colors hover:text-white/60 underline-offset-4">@陈刑</a>
+	<footer class="px-6 pb-6 text-xs sm:px-10 md:px-16 lg:px-24 xl:px-32">
+		<a href="https://github.com/chenxing-dev" class="transition-colors text-white/40 hover:text-white/60 underline-offset-4">@陈刑</a>
 	</footer>
 </div>
